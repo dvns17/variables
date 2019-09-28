6e100a2e2c4cb30e6ad30b31bbc207400775dbd4
Danica Washington <55327173+dvns17@users.noreply.github.com>
2019-09-28 11:05:17 -0500

    Initial commit

---
+++ variableDrills.py
@@ -0,0 +1,109 @@
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a variable called name and assign it to your name. And print the variable.
+name = "Danica"
+print(name)
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+'''INTEGERS'''
+var1 = 5
+
+print var1
+
+var2 = 10
+
+print var2
+
+var3 = var2
+
+print var3
+
+x = 14
+
+print x
+
+y = 100
+
+print y 
+
+'''FLOATS'''
+z = 1.1
+
+print z 
+
+a = 2.75
+
+print a 
+
+b = a
+
+print b 
+
+c = 100.1
+
+print c 
+
+d = 12.3
+
+print d 
+
+'''STRINGS'''
+name = Danica 
+
+print name 
+
+e = D
+
+print e 
+
+f = A
+
+print f 
+g = "chicken"
+
+print g 
+
+h = "Dan"
+
+print h 
+
+'''BOOLEANS'''
+done = False
+
+print done 
+
+smart = True
+
+print smart 
+
+i = done
+
+print i 
+
+j = False
+
+print j 
+
+almostDone = True
+
+print almostDone
+
+'''LISTS'''
+listInt = [1, 2, 3, 4, 5,]
+
+print listInt [4]
+
+listString ["6", '7", "8", "9", "10"]
+
+print listString ["10"]
+
+listBoolean [False, True, True, False, True]
